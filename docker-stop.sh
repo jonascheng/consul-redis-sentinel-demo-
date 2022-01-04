1bf2214cbea5de67f734abb488524e986257565a
Jonas Cheng <jonas_cheng@trendmicro.com>
2022-01-04 19:24:23 +0800

    feat(docker): indivisual mode

--- docker-stop.sh
+++ docker-stop.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-OS="$(uname -s)"
-
-case "${OS}" in
-    Darwin*)    HOSTIP=`ipconfig getifaddr en0` docker-compose stop;;
-    *)          HOSTIP=`hostname --ip-address` docker-compose stop;;
-esac
+docker-compose stop
